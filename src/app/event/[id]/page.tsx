3ca2797e308bf8a868937011eecb0aa5e4aa1a93
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-11 09:14:54 -0300

    Criando página do evento

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -2,9 +2,17 @@ type Props = { params: { id: string } };
 
 const Page = ({ params }: Props) => {
     return (
-        <div>
-            ID DO EVENTO: {params.id}
-        </div>
+        <main className="text-center mx-auto max-w-lg p-5">
+            <header>
+                <h2 className="text-2xl text-yellow-400">Amigo Secreto</h2>
+                <h1 className="text-3xl mt-5 mb-2">** TITULO DO EVENTO **</h1>
+                <p className="text-sm mb-5">** DESCRIÇÃO DO EVENTO **</p>
+            </header>
+
+            ...
+
+            <footer className="mt-5 text-sm">Criado por @jão.luisrm_</footer>
+        </main>
     );
 };
 
