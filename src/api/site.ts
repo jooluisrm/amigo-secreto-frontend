8497af1d68fb4cf0a91b960f52251aa1418afe4d
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-11 09:59:14 -0300

    Consultando o evento (API ERRO)

--- src/api/site.ts
+++ src/api/site.ts
@@ -1,2 +1,7 @@
 import { req } from '@/api/axios';
+import { Event } from '@/types/Event';
 
+export const getEvent = async (id: number): Promise <Event | false> => {
+    const json = await req.get(`/events/${id}`);
+    return json.data.event as Event ?? false;
+}
--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -1,12 +1,22 @@
+import * as api from "@/api/site";
+import { redirect } from "next/navigation";
+
 type Props = { params: { id: string } };
 
-const Page = ({ params }: Props) => {
+const Page = async ({ params }: Props) => {
+
+    const eventItem = await api.getEvent(parseInt(params.id));
+
+    
+
+    if(!eventItem || !eventItem.status) return redirect('/');
+    
     return (
         <main className="text-center mx-auto max-w-lg p-5">
             <header>
                 <h2 className="text-2xl text-yellow-400">Amigo Secreto</h2>
-                <h1 className="text-3xl mt-5 mb-2">** TITULO DO EVENTO **</h1>
-                <p className="text-sm mb-5">** DESCRIÇÃO DO EVENTO **</p>
+                <h1 className="text-3xl mt-5 mb-2">{eventItem.title}</h1>
+                <p className="text-sm mb-5">{eventItem.discription}</p>
             </header>
 
             ...
